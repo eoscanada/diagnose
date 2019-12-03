1c858c7f88da0bd9d6d43f45a478733959276a95
Julien Cassis <juliencassis@gmail.com>
2019-12-03 18:31:15 -0500

    fix localhost

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,5 @@
-export const API_URL = `${process.env.HOSTNAME || "localhost:8080"}/api/`;
+export const API_URL =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "localhost:8080/api/"
+    : `${window.location.hostname}/api/`;
+console.log("Api: ", API_URL);
